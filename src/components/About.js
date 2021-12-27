514595f4b3964cf975c676cf2a43336c17dfc2b6
zerocool6998 <anas.nouar@usms.ac.ma>
2021-12-27 20:56:02 +0000

    adding tailwind css and building About comp

--- src/components/About.js
+++ src/components/About.js
@@ -30,7 +30,7 @@ export default function About() {
                     <img
                         className="object-cover object-center rounded"
                         alt="hero"
-                        src="./coding.svg"
+                        src="./Anas.jpg"
                      />
                 </div>
             </div>
